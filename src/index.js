7c6dd91a2660cc8d5218bf89b248f53a53e496e1
namkijoo <rlwn5904@naver.com>
2024-08-10 03:19:19 +0900

    pwa설정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./service-worker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
